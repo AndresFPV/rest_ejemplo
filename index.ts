5e0395e2f4eafddf0776f647e00ff07bffb5104e
ANDRES PORRAS VERGARAY <andresporras@MacBook-Pro-de-ANDRES-2.local>
2020-02-05 17:27:59 -0500

    Ejemplo terminado

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
 import * as express from "express"
 
 let app : express.Application = express()
+
+interface Alumno
+{
+    codigo : string
+    nombre : string
+}
+
+//Recurso Alumnos
+// /alumnos/20142153
+app.get("/alumnos/:codigo",(req,res) => {
+    let alumno :Alumno = {
+        codigo : "20142153",
+        nombre : "Andres"
+    }
+    res.send(alumno)
+})
+
+app.listen(3000, () => {
+    console.log("Servidor ejecutandose...")
+})
